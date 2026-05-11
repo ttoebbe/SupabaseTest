2720bd2535ecbe9d5c1015fa18d67f567da4d525
Thomas Többe <toebbe.thomas@outlook.de>
2026-05-11 12:21:00 +0200

    feat: get first data from supabase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Supabase } from './supabase';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   dbService = inject(Supabase);
+
+  ngOnInit(){
+    this.dbService.getProducts();
+  }
 }
--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { createClient } from '@supabase/supabase-js'
 
 @Injectable({
@@ -9,4 +9,19 @@ export class Supabase {
 supabaseUrl = 'https://sgeldqqjbsxugotjiilw.supabase.co'
 supabaseKey = 'sb_publishable_pZRR_Cwb2pLRUrzCVmZH7Q_Rw7Fl7Db'
 supabase = createClient(this.supabaseUrl, this.supabaseKey)
+
+products = signal<{id:number, created_at:string, name:string, count:number}[]>([]);
+
+async getProducts() {
+let { data: products, error } = await this.supabase
+  .from('products')
+  .select('*')
+
+  if(!products) {
+    console.error(error);
+    return;
+  }
+  this.products.set(products);
+}
+        
 }
