75661b74c74d9071917b5bd6890cb19dabc1e327
Thomas Többe <toebbe.thomas@outlook.de>
2026-05-11 14:17:45 +0200

    feat: filter

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -10,12 +10,14 @@ supabaseUrl = 'https://sgeldqqjbsxugotjiilw.supabase.co'
 supabaseKey = 'sb_publishable_pZRR_Cwb2pLRUrzCVmZH7Q_Rw7Fl7Db'
 supabase = createClient(this.supabaseUrl, this.supabaseKey)
 
-products = signal<{id:number, created_at:string, name:string, count:number}[]>([]);
+products = signal<{ name:string, count:number}[]>([]);
 
 async getProducts() {
 let { data: products, error } = await this.supabase
   .from('products')
-  .select('*')
+  .select('name, count')
+  .range(0, 9)
+  .lte('count', 50)
 
   if(!products) {
     console.error(error);
